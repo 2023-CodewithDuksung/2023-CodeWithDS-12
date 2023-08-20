ec8993e437ad32618de444865c1b9b9d7a59b5df
Eunbeann <ds5bean23@gmail.com>
2023-08-21 01:28:38 +0900

    [ init ] 라우팅 구현

--- Frontend/duksiri/.eslintrc.cjs
+++ Frontend/duksiri/.eslintrc.cjs
@@ -1,20 +1,28 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  plugins: ["react", "prettier"],
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "react-app",
+    "plugin:prettier/recommended",
+    "airbnb",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:prettier/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "linebreak-style": 0,
+    "prettier/prettier": 0,
+    "import/extensions": 0,
+    "import/no-unresolved": 0,
+    "import/no-extraneous-dependencies": 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    "import/prefer-default-export": 0,
+    "react/prop-types": 0,
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
+    "jsx-a11y/no-noninteractive-element-interactions": 0,
+    "eol-last": ["error", "always"], // line의 가장 마지막 줄에는 개행 넣기
+    "simple-import-sort/imports": "error", // import 정렬
+    "no-multi-spaces": "error"
   },
-}
+};
---
+++ Frontend/duksiri/src/pages/ConditionalPlan.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ConditionalPlan() {
+  return <div>ConditionalPlan</div>;
+}
