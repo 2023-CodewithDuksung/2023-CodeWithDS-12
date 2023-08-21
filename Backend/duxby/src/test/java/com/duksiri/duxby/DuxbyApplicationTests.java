4e7718117c57815d6762e273af03f50e96f65c6f
sumin <ski03257@naver.com>
2023-08-21 13:05:21 +0900

    프로젝트 생성

---
+++ Backend/duxby/src/test/java/com/duksiri/duxby/DuxbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duksiri.duxby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuxbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
