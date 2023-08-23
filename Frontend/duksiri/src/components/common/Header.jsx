60332dcfdb10387378a97a6941979d655c38bb07
Eunbeann <ds5bean23@gmail.com>
2023-08-24 00:41:38 +0900

    [ feat ] 덕성 로고 추가;

--- Frontend/duksiri/src/components/common/Header.jsx
+++ Frontend/duksiri/src/components/common/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { styled } from 'styled-components';
+import DuksungLogoWithTitle from '../../assets/icon/DuksungLogoWithTitle.png';
 
 export default function Header() {
-  return <HeaderWrapper>header</HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <DuksungLogo src={DuksungLogoWithTitle} />
+    </HeaderWrapper>
+  );
 }
 
 const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
   position: fixed;
   top: 0;
   z-index: 1;
@@ -14,4 +21,14 @@ const HeaderWrapper = styled.div`
   height: 8rem;
 
   background-color: #fff;
+`;
+
+const DuksungLogo = styled.img`
+  width: 17.6rem;
+  height: 4rem;
+  margin-left: 2.6rem;
+`;
+
+const UserContainer = styled.div`
+  display: flex;
 `;
