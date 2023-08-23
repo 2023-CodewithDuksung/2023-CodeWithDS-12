485b987225d1c01dd0c1839a347141f0d34815e2
Eunbeann <ds5bean23@gmail.com>
2023-08-24 01:00:00 +0900

    [ style ] map으로 리팩토링

--- Frontend/duksiri/src/components/common/Header.jsx
+++ Frontend/duksiri/src/components/common/Header.jsx
@@ -3,15 +3,39 @@ import { styled } from 'styled-components';
 import DuksungLogoWithTitle from '../../assets/icon/DuksungLogoWithTitle.png';
 
 export default function Header() {
+  const TOP_NAV_LIST = ['김덕성', '덕성홈페이지', '이클래스', '포탈'];
+
+  function handleMoveSite(site) {
+    switch (site) {
+      case '덕성홈페이지':
+        window.open('http://www.duksung.ac.kr/');
+        break;
+      case '이클래스':
+        window.open('https://lms.duksung.ac.kr/');
+        break;
+      case '포탈':
+        window.open('https://sso.duksung.ac.kr/');
+        break;
+    }
+  }
   return (
     <HeaderWrapper>
-      <DuksungLogo src={DuksungLogoWithTitle} />
+      <DuksungLogo src={DuksungLogoWithTitle} alt="duksungLogo" />
+      <UserContainer>
+        {TOP_NAV_LIST.map((site, idx) => (
+          <TopList key={idx} onClick={() => handleMoveSite(site)}>
+            {menu}
+          </TopList>
+        ))}
+        <LogButton>로그아웃</LogButton>
+      </UserContainer>
     </HeaderWrapper>
   );
 }
 
 const HeaderWrapper = styled.div`
   display: flex;
+  justify-content: space-between;
   align-items: center;
   position: fixed;
   top: 0;
@@ -29,6 +53,27 @@ const DuksungLogo = styled.img`
   margin-left: 2.6rem;
 `;
 
-const UserContainer = styled.div`
+const UserContainer = styled.ul`
   display: flex;
+  align-items: center;
+`;
+
+const UserIcon = styled.img``;
+
+const TopList = styled.li`
+  margin-left: 2rem;
+
+  color: #808080;
+  font-weight: 500;
+  font-size: 1.6rem;
+`;
+
+const LogButton = styled.button`
+  margin: 0 3rem;
+  padding: 0.8rem 1.5rem;
+
+  background: #808080;
+  color: #fff;
+  font-weight: 500;
+  font-size: 1.6rem;
 `;
