a3f32400c93f8d6b5e4a996d487e224e91a8c0cf
imddoy <imddoy@duksung.ac.kr>
2023-08-23 14:50:28 +0900

    [design] navigation 수정

--- Frontend/duksiri/src/components/common/SideNavigation.jsx
+++ Frontend/duksiri/src/components/common/SideNavigation.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 export default function SideNavigation() {
   const navigate = useNavigate();
-  const MenuList = ['학생 정보', '추가 정보', '이수 내역', '플래너', '추천 강의'];
+  const MenuList = ['학생 정보', '이수 내역', '커리어 플래너', '스마트 시간표', '추천 강의'];
 
   function handleMovePage(menu) {
     switch (menu) {
@@ -13,7 +13,10 @@ export default function SideNavigation() {
       case '이수 내역':
         navigate('/course');
         break;
-      case '플래너':
+      case '커리어 플래너':
+        navigate('/career');
+        break;
+      case '스마트 시간표':
         navigate('/plan');
         break;
       default:
@@ -57,10 +60,10 @@ const MenuUl = styled.ul`
 const MenuLi = styled.li`
   width: 23rem;
   margin: 0.3rem;
-  padding: 2rem 7rem;
+  padding: 2rem 5rem;
 
   font-size: 1.8rem;
-
+  text-align: center;
   &:hover {
     cursor: pointer;
   }
