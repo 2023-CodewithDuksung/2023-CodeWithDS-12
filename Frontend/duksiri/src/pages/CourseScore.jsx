f1b2b94bc2f30de7e0f820a6a119a798d800202c
Eunbeann <ds5bean23@gmail.com>
2023-08-25 00:15:53 +0900

    vite-plugin-svg 설치

--- Frontend/duksiri/src/pages/CourseScore.jsx
+++ Frontend/duksiri/src/pages/CourseScore.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styled } from 'styled-components';
 import ElectiveClass from '../components/courseScore/ElectiveClass';
 import FirstMajor from '../components/courseScore/FirstMajor';
 import { USER_DATA } from '../core/mockCourseScore';
 import Graduate from '../components/courseScore/graduate';
 import SecondMajor from '../components/courseScore/SecondMajor';
+import { UnderTriangle } from '../assets';
 
 export default function CourseScore() {
   const userData = USER_DATA.persondata;
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <CourseWrapper>
       <Title>이수 내역</Title>
       <BoxContainer>
         <Graduate userData={userData} />
+        <UnderTriangleIcon />
         <ElectiveClass userData={userData} />
         <FirstMajor userData={userData} />
         <SecondMajor userData={userData} />
@@ -35,4 +39,8 @@ const Title = styled.h1`
 const BoxContainer = styled.article`
   display: flex;
   flex-direction: column;
+`;
+const UnderTriangleIcon = styled(UnderTriangle)`
+  width: 3rem;
+  height: 1%.5;
 `;
